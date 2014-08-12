66de8307b834f661e7570b00159d3506d72551f4
skyler <skyler@opendns.com>
2014-08-11 17:21:41 -0700

    Complete setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
-from os import path
+import os
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='investigate',
-    version='0.0.0',
-    description='Interface for the OpenDNS Investigate API',
+    version='1.0.0',
+    description='Python interface for the OpenDNS Investigate API',
+    long_description=read("README.md"),
     url='https://github.com/dead10ck/pyinvestigate.git',
     author='Skyler Hawthorne',
     author_email='skylerhawthorne@gmail.com',
     license='MIT',
     keywords='opendns investigate',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=required,
 )
