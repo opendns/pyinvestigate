703fbb824bc2d266b27d247d66a15fc7bcc3b563
skyler <skyler@opendns.com>
2014-11-13 09:31:58 -0800

    Fix setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from codecs import open  # To use a consistent encoding
-import os
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='investigate',
     version='1.0.0',
     description='Python interface for the OpenDNS Investigate API',
-    long_description=read("README.md"),
     url='https://github.com/dead10ck/pyinvestigate.git',
     author='Skyler Hawthorne',
     author_email='skylerhawthorne@gmail.com',
     license='MIT',
     keywords='opendns investigate',
     packages=find_packages(),
-    install_requires=required,
+    install_requires=['requests'],
 )
