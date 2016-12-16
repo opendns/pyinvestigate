8f06ce2ae74134843d16ad18b128339815371ae0
Skyler Hawthorne <skyler@opendns.com>
2016-12-16 13:47:07 -0800

    Do testing with tox
    Switch testing to tox to take care of setting up an environment with all
    required dependencies to run the tests. You still need the
    INVESTIGATE_KEY environment variable set for the tests to work.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
+import os
 
 import pytest
 import investigate # use the local path, instead of what has been installed
