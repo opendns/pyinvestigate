3b07406ca997a5102f73396ab5e91354acd42581
pranbhav <pranbhav@cisco.com>
2020-02-26 15:32:30 -0800

    Adding new risk-score and bug fix for paramters in pdns endpoints

--- tests/test_investigate.py
+++ tests/test_investigate.py
@@ -616,7 +616,15 @@ def test_risk_score(inv):
     resp_json = inv.risk_score('bibikun.ru')
 
     search_keys = [
-        'risk_score'
+        'riskScore',
+        'features'
+    ]
+
+    features_keys = [
+        'feature',
+        'normalizedScore',
+        'score'
     ]
 
     assert_keys_in(resp_json, *search_keys)
+    assert_keys_in(resp_json['features'][0], *features_keys)
